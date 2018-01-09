b98838ec72fdda29655c3e7d3e8e52824b46dd93
MarCalmon <marical2222@gmail.com>
2018-01-08 21:14:54 -0300

    creación de Menu.js y cambios en Header.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
+//Dependencies
 import React, { Component } from 'react';
-
 
 //Components
 import Header from './Global/Header';
 import Content from './Global/Content';
 import Footer from './Global/Footer';
 
+//Data
+import items from '../data/menu';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header title="Mar Calmón" items={items} />
         <Content />
         <Footer />
       </div>
--- src/components/Global/Header.js
+++ src/components/Global/Header.js
@@ -1,14 +1,28 @@
+//Dependencies
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+//Assets
 import logo from './img/logo.svg';
 import './css/Header.css';
 
 class Header extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    items:PropTypes.array.isRequired
+  };
   render() {
+    const{ title, items} = this.props;
+
     return (
       <div className="Header">
         <header className="Logo">
           <img src={logo} alt="logo" />
-          <h1 className="App-title">Hello World React</h1>
+          <h2>{title}</h2>
+
+          <ul className="Menu">
+            {items && items.map((item, key) => <li key={key}>{item.title}</li>)}
+          </ul>
         </header>
       </div>
     );
