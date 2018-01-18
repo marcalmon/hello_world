3916c3c8ae1ca9cb85698997ad1f7041b85aaca0
MarCalmon <marical2222@gmail.com>
2018-01-18 07:38:06 -0300

    enlazando con Link los componentes react de nuestro Menu

--- src/components/Global/Content.js
+++ src/components/Global/Content.js
@@ -8,61 +8,6 @@ class Content extends Component {
   static propTypes = {
     body: PropTypes.object.isRequired
   };
-  /*constructor(){
-  	super();
-
-  	this.state = {
-  		count: 0,
-  		number1: 0,
-  		number2:0,
-  		result: 0
-  	};
-
-  	this.handleCountClick = this.handleCountClick.bind(this);
-  	this.handleResultClick = this.handleResultClick.bind(this);
-  	this.handleInputChanged = this.handleInputChanged.bind(this);
-  }
-
-  componentDidMount(){
-  	this.setState({
-  		count: 1
-  	});
-  }
-
-  handleCountClick(e){
-  	if(e.target.id === 'add'){
-  		this.setState({
-  			count: this.state.count + 1
-  		});
-  	}else if(e.target.id === 'subtract' && this.state.count>0){
-  		this.setState({
-  			count: this.state.count - 1
-  		});
-  	}else{
-  		this.setState({
-  			count: 0
-  		});
-  	}
-  }
-
-  handleResultClick(e){
-  	this.setState({
-  		result: this.state.number1 + this.state.number2
-  	});
-  }
-
-  handleInputChanged(e){
-  	if(e.target.id === 'number1'){
-  		this.setState({
-  			number1: Number(e.target.value)
-  		});
-  	}else{
-  		this.setState({
-  			number2: Number(e.target.value)
-  		});
-  	}
-  }*/
-
   render() {
   	//console.log("Ejecuta Render");
     const { body } = this.props;
--- src/components/Global/Header.js
+++ src/components/Global/Header.js
@@ -1,6 +1,7 @@
 //Dependencies
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 //Assets
 import logo from './img/logo.svg';
@@ -21,7 +22,9 @@ class Header extends Component {
           <h2>{title}</h2>
 
           <ul className="Menu">
-            {items && items.map((item, key) => <li key={key}>{item.title}</li>)}
+            {
+              items && items.map(
+              (item, key) => <li key={key}><Link to={item.url}>{item.title}</Link></li>)}
           </ul>
         </header>
       </div>
